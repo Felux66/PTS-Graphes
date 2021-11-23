2a51c7b81f3e830f5ffce349192d344b5f2a69ee
ConanGuy <mathieuhuin@free.fr>
2021-11-23 10:32:50 +0100

    Added explanations to implement new algos

--- ColoringAlgos.py
+++ ColoringAlgos.py
@@ -2,6 +2,38 @@ import random
 from consts import *
 from pysat.solvers import Minisat22
 
+"""
+
+ASTUCES POUR IMPLEMENTER UN NOUVEL ALGO:
+
+    Le gui comporte un élément principal GraphGUI qui contient:
+        vertices: liste des VertexGUI (sommets pour l'interface)
+        edges: listes des couples (v1, v2) avec v1 et v2 les deux VertexGUI qui composent l'arête
+        graph: le graph sous la forme [0: [1,3], 1: [0,2,3], ...]
+
+    Créez un méthode principale dans la classe pour lancer l'algo qui prend en paramètre le GraphGUI
+    Travaillez ensuite avec des fonctions à l'intérieur de la méthode comme dans l'exemple du SAT
+
+    Vous pouvez identifier un VertexGUI en le comparant avec un string ou un VertexGUI, c'est le nom 
+    du sommet qui sera comparé.
+
+    Pour modifier la couleur sur l'interface, il suffit de faire vertex.color = new_color avec:
+        vertex: le VertexGUI
+        new_color: le nom de la couleur (ex: red, green, ...)
+
+    Dans l'exemple du SAT, Félix a travaillé sur le graphe en liste. Il a juste fallu modifier la
+    couleur en parcourant la liste des VertexGUI en les identifiant par leur nom (cf. l'algo) 
+
+    Dans l'exemple de COLOR, j'ai travaillé directement sur le GraphGUI, cela marche tout aussi bien, quoique
+    peut-être un peu plus complexe si vous n'êtes pas à l'aise avec le code
+
+
+PENSEZ A AJOUTER VOTRE ALGO DANS LE DICTIONNAIRE algos A LA FIN DU CODE AVEC:
+    key: le nom de l'algo qui va apparaître dans l'interface
+    value: la méthode principale qui sera appelée pour lancer l'algo
+
+"""
+
 class ColoringAlgos:
 
     def sat(graph):
@@ -20,7 +52,6 @@ class ColoringAlgos:
             m=n-1
             ne = len(e)
             return (e[m % ne], c[m//ne])
-
 
         def global_sat(graphe):
 
@@ -63,6 +94,10 @@ class ColoringAlgos:
 
         global_sat(graph)
 
+    ##############################
+    ##############################
+    ##############################
+
     def color(graph, current=None, visited=None):
         if visited == None:
             visited = []
@@ -91,6 +126,10 @@ class ColoringAlgos:
         
         if not hasNone:
             ColoringAlgos.color(graph,None,visited)
+
+    ##############################
+    ##############################
+    ##############################
 
     algos = {
         "COLOR": color,
